70614d089588530e7ca9a0f713ab88be6ca5057b
sachuu197 <71245862+sachuu197@users.noreply.github.com>
2021-07-27 15:08:24 +0530

    From git c# application

--- PrjCommandLineApplication/PrjFirstApplication/FirstOne/Program.cs
+++ PrjCommandLineApplication/PrjFirstApplication/FirstOne/Program.cs
@@ -7,6 +7,7 @@ namespace FirstOne
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Amaravati!!!");
         }
     }
 }
