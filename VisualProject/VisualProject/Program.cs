e65078ac35aba45014ecf9e3d31b424c524d78d7
sachuu197 <sachinghuge865@gmail.com>
2021-07-28 10:51:45 +0530

    from visual

---
+++ VisualProject/VisualProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
