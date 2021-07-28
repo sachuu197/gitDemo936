5ab7573ae9df871621797fe68167d0c3357e23c9
sachuu197 <sachinghuge865@gmail.com>
2021-07-28 10:54:46 +0530

    helloBoyz

--- VisualProject/VisualProject/Program.cs
+++ VisualProject/VisualProject/Program.cs
@@ -7,6 +7,8 @@ namespace VisualProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Boyz!");
+
         }
     }
 }
